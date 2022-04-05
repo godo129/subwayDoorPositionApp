3eba5d4a6424ec69ab0062343501af1899d3ecbf
godo <godo129@naver.com>
2022-04-06 01:55:08 +0900

    위치 정보 권한 부여

--- subwayDoorPositionApp/TravelInfoViewController.swift
+++ subwayDoorPositionApp/TravelInfoViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import CoreLocation
 
 class TravelInfoViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    var currentLocation = CLLocationCoordinate2D()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        locationManager.delegate = self
+        getLocationUsagePermission()
     }
 
 
+}
+
+extension TravelInfoViewController: CLLocationManagerDelegate {
+    
+    func getLocationUsagePermission() {
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways :
+            print("권한 설정 됨")
+        case .restricted, .notDetermined :
+            print("권한 설정 되지 않음")
+        case .denied :
+            print("권한 요청 거부 됨")
+        default :
+            print("GPS, Default")
+            
+        }
+        
+    }
 }
