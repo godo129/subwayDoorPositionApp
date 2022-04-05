e605984d93c04bef02e420e62f937ecb94b7deb3
godo <godo129@naver.com>
2022-04-06 05:03:01 +0900

    annotation 이름들 나오게 하기 ... title은 제목, subtitle은 클릭했을 때 title 밑에 나오게 됨

--- subwayDoorPositionApp/model/stopInfo.swift
+++ subwayDoorPositionApp/model/stopInfo.swift
@@ -15,6 +15,8 @@ class stopInfo: NSObject, MKAnnotation {
     let doorPosition : String?
     let heading : String?
     
+    let title: String? // 어노테이션 이름 
+    
     init(name: String?, lineName: String?, coordinate: CLLocationCoordinate2D, doorPosition: String?, heading: String?) {
         self.name = name
         self.lineName = lineName
@@ -22,11 +24,13 @@ class stopInfo: NSObject, MKAnnotation {
         self.doorPosition = doorPosition
         self.heading = heading
         
+        self.title = name // 어노테이션 이름 나오게 함
+        
         super.init()
     }
     
-    var title: String? {
-        return name
+    var subtitle: String? { // 클릭하면 어노테이션 나오게 함
+        return heading
     }
 
 }
