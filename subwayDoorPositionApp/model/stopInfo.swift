ceb0ebb90775404d337fc4272968ddc35d31ef61
godo <godo129@naver.com>
2022-04-06 04:29:31 +0900

    역 정보 모델 재설정

--- subwayDoorPositionApp/model/stopInfo.swift
+++ subwayDoorPositionApp/model/stopInfo.swift
@@ -5,18 +5,28 @@
 //  Created by hong on 2022/04/05.
 //
 
-import UIKit
+import MapKit
 
-class stopInfo {
+class stopInfo: NSObject, MKAnnotation {
     
-    var name : String!
-    var lineName : String!
-    var pointPosition : String!
+    let name : String?
+    let lineName : String?
+    let coordinate : CLLocationCoordinate2D
+    let doorPosition : String?
+    let heading : String?
     
-    init(name: String, lineName: String) {
+    init(name: String?, lineName: String?, coordinate: CLLocationCoordinate2D, doorPosition: String?, heading: String?) {
         self.name = name
         self.lineName = lineName
+        self.coordinate = coordinate
+        self.doorPosition = doorPosition
+        self.heading = heading
         
+        super.init()
+    }
+    
+    var title: String? {
+        return name
     }
 
 }
