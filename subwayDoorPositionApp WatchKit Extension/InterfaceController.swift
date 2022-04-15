a9bba9e865d9bf821ff217cbb3e4dffe790983dc
godo <godo129@naver.com>
2022-04-15 23:58:49 +0900

    버튼추가

--- subwayDoorPositionApp WatchKit Extension/InterfaceController.swift
+++ subwayDoorPositionApp WatchKit Extension/InterfaceController.swift
@@ -7,14 +7,21 @@
 
 import WatchKit
 import Foundation
-
+import UIKit
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var SubwayInfoButton: WKInterfaceButton!
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        
+        SubwayInfoButton.setTitle("지하철 정보")
     }
     
+    @IBAction func SubwayInfoButtonTapped() {
+        presentController(withName: "subwayInfo", context: .none)
+    }
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
     }
